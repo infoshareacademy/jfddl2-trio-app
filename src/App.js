fff29670bf21f012e75f81243ceb65089df7cbbd
NaukaGita <jung-c@o2.pl>
2017-10-05 16:26:25 +0200

    PersonDetails

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PersonDetails from './PersonDetails'
+
 
 class App extends Component {
   render() {
