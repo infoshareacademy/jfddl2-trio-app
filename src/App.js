fcc5cbc7c5dd21508a3459923f0d24cb4cbddefb
Bartek <bartoszlis87@gmail.com>
2017-10-05 16:55:46 +0200

    Add Browser Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {
+    BrowserRouter as Router,
+    Route,
+} from 'react-router-dom'
+import {
+    Grid
+} from 'react-bootstrap'
 class App extends Component {
   render() {
     return (
