28dfb32dc8aa067d30a37fbbc517821bb03cffd9
NaukaGita <jung-c@o2.pl>
2017-10-08 14:38:50 +0200

    no problem

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
     return (
         <Router>
             <Grid>
-                <PersonDetails/>
+                <Person/>
             </Grid>
         </Router>
     );
